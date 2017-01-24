435859379a47d1054d4f44bb0616516f8d847862
Josephine-Chen <jychen@g.hmc.edu>
2017-01-24 15:53:37 -0800

    Modals working

--- client/app/champion/champion.js
+++ client/app/champion/champion.js
@@ -1,28 +1,4 @@
-var app = angular.module('mvp.champion', [])
-
-app.directive('modalDialog', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      show:'='
-    },
-    replace:true,
-    transclude:true,
-    link: function(scope, element, attrs) {
-      scope.dialogStyle = {};
-      if (attrs.width) {
-        scope.dialogStyle.width = attr.width;
-      } else if (attrs.height) {
-        scope.dialogStyle.height = attrs.height;
-      }
-      console.log('scope is', scope);
-      scope.hideModal = function() {
-        scope.show = false;
-      };
-    },
-    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
-  };
-});
+var app = angular.module('mvp.champion', ['ui.materialize'])
 
 app.controller('ChampionController', function($scope, $http, $location, Champions) {
   var champ = $location.$$path.split('/')[2];
@@ -37,10 +13,4 @@ app.controller('ChampionController', function($scope, $http, $location, Champion
       console.log('error getting scope data champions', err);
     });
     }();
-
-    $scope.modalShown = false;
-    $scope.toggleModal = function(skin) {
-      $scope.skin = skin;
-      $scope.modalShown = !$scope.modalShown;
-    };
 });
